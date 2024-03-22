af95f84c2d8cd6bbc2d64816cf7abef0671172a1
Thiago Regueira <thiago.regueira@yahoo.com.br>
2024-03-22 16:35:14 -0300

    Atualizações no arquivo app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ st.write(
     """<h4>Graduação em Engenharia de Dados | PUC-Minas</h4>""", unsafe_allow_html=True
 )
 st.write("08/2023 - 12/2025" + " | " + "Graduando")
-st.write("#")
+
 st.write("<h4>Pós-Graduação em Banco De Dados | FASUL-MG</h4>", unsafe_allow_html=True)
 st.write("05/2023 - 08/2023" + " | " + "Pós-Graduado")
 st.download_button(
