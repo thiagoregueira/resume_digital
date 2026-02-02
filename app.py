2fb2799b322a44f58d8519390c34e2baffbda5c0
thiagoregueira <thiago.regueira@yahoo.com.br>
2026-02-02 08:55:26 -0300

    fix: atualiza o link do notebook Titanic para o Google Colab.

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ PROJECTS_DATA = {
     '🏅 Cotações de moedas em relação ao real': 'https://cotacaomoedas.streamlit.app/',
     '🏅 Consultar eventos históricos que ocorreram em uma data específica': 'https://eventoshistoricos.streamlit.app/',
     '🏅 Quadro de medalhas geral das Olimpíadas': 'https://medalhasolimpicas.streamlit.app/',
-    '🏅 Notebook Análise Exploratória Dataset Titanic': 'https://drive.google.com/file/d/1Y1Fa5YKhzadU1vm4GYo3-uyD3sjaDT93/view?usp=sharing',
+    '🏅 Notebook Análise Exploratória Dataset Titanic': 'https://colab.research.google.com/drive/1Y1Fa5YKhzadU1vm4GYo3-uyD3sjaDT93',
     '🏅 Notebook Análise de Exploratória e Ciência de dados': 'https://colab.research.google.com/drive/10_y-rR-Dvo2L7RhTj7MrMEPHWP3Q4hSd?usp=sharing',
     '🏅 Notebook Prevendo Temperaturas': 'https://colab.research.google.com/drive/1OxNg2gEXTA8Rzja9HSS_hq2XCqxWQTLz?usp=sharing',
     '🏅 Notebook Prevendo valores de Ações': 'https://colab.research.google.com/drive/19Ia_c8tGGbjcmwoErk3LzeXoKlJNuHsX?usp=sharing',
